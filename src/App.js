552497723786218fad1d6b17c027004c7a4b3f2d
Ardi Jorganxhi <ardijorganxhi@eskisehir.edu.tr>
2023-05-03 14:02:04 +0300

    implement react-router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/home';
+import Upload from './pages/upload';
+import Result from './pages/result';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/upload' element={<Upload />}></Route>
+      <Route path='/result' element={<Result />}></Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
