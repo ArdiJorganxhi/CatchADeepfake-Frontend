6ecdf296e4cc5ef6d5af0be270181ab027139ead
Ardi Jorganxhi <ardijorganxhi@eskisehir.edu.tr>
2023-05-04 16:44:22 +0300

    implement upload page

--- src/pages/upload.jsx
+++ src/pages/upload.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import Navbar from '../components/navbar'
+import './css/upload.css'
+import Button from '../components/button'
 
 export default function Upload() {
   return (
     <div>
       <Navbar />
+      <div className="upload-container">
+      <div className="upload-title">
+        <h3>Upload your video or audio file.</h3>
+      </div>
+      <div className="buttons-container">
+        <Button title="Upload video" />
+        <Button title="Upload audio" />
+      </div>
+      </div>
+      
     </div>
   )
 }
